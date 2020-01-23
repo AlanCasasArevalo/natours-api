01f23bfe6d665ed209e26e368c3e4f8c4167862f
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-01-23 19:00:16 +0100

    dotenv was added to configure Node enviroments

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, '../public');
-
 app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
-
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
