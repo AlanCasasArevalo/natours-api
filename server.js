b0f3df71606350cfefd658a0374b4bf7b42f5e2a
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-01-25 18:49:56 +0100

    Mongoose connection was implemented

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({
     path: './config.env'
@@ -8,6 +9,29 @@ const http = require('http');
 let server = http.createServer(app);
 // IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
+
+let DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// if (process.env.NODE_ENV === 'development') {
+//     DB = process.env.DATABASE_LOCAL
+// } else {
+//     DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// }
+
+mongoose
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then((connection) => {
+        // console.log('Mongoose connection', connection.connections );
+        console.log('DB connection SUCCESSFUL!!');
+    });
+
+
+
+
 
 
 const port = process.env.PORT || 3000;
