d1e2c7bdf3d57c3450b7855f399136aec6efe320
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-01-26 10:56:35 +0100

    Get all tours was implemented with data base

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,33 @@
-// const fs = require('fs');
-// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 const Tour = require('./../models/tourModel');
 
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    })
+const getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        if (tours && typeof tours !== 'undefined') {
+            res.status(200).json({
+                status: 'success',
+                results: tours.length,
+                data: {
+                    tours
+                }
+            })
+        } else {
+            res.status(500).json({
+                status: 'failed',
+                message: 'The recovered tours was failed'
+            })
+        }
+    } catch (error) {
+        res.status(404).json({
+            status: 'failed',
+            message: 'The recovered tours was failed'
+        })
+    }
 };
 
 const createNewTour = async (req, res) => {
     try {
         const newTour = await Tour.create(req.body);
-
         if (newTour && typeof newTour !== 'undefined'){
             res.status(201).json({
                 status: 'success',
