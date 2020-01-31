ae0f9c4bfd70e83b0593a01dc34e39881a55b5e6
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-01-31 20:02:27 +0100

    New middleware was implemented to get top 5 cheaper tours with some requirements

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -166,10 +166,20 @@ const deleteTour = async (req, res) => {
         })
     }
 };
+
+const aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingAverage, price';
+    req.query.fields = 'name, price, ratingAverage, summary, difficulty';
+
+    next()
+};
+
 module.exports = {
     getAllTours,
     createNewTour,
     updateTour,
     getTour,
     deleteTour,
+    aliasTopTours,
 };
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -4,6 +4,13 @@ const tourController = require('../controllers/tourController');
 
 // router.param('id', tourController.checkId);
 
+// Alias to get
+router.route('/top-5-cheap')
+    .get(
+        tourController.aliasTopTours,
+        tourController.getAllTours
+    );
+
 router.route('/')
     .get(tourController.getAllTours)
     .post(
