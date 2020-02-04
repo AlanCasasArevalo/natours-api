50a0c6240ea1f0d6f9e4d2563c7a47ca232a91e5
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-02-04 19:22:38 +0100

    Custom friendly message to user

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleErrorDB = errorDB => {
+    const message = `Invalid ${errorDB.path}: ${errorDB.value}`;
+    return new AppError(message, 400);
+};
+
 const developmentError = (error, res) => {
     const statusErrorCode = error.statusCode || 500;
     const status = error.status || 'error';
@@ -10,12 +17,9 @@ const developmentError = (error, res) => {
     })
 };
 const productionsError = (error, res) => {
-    const statusErrorCode = error.statusCode || 500;
-    const status = error.status || 'error';
-
     if (error.isOperational) {
-        res.status(statusErrorCode).json({
-            status: status,
+        res.status(error.statusCode).json({
+            status: error.statusCode,
             message: error.message
         })
     } else {
@@ -32,7 +36,10 @@ const globalErrorHandler = (error, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         developmentError(error, res);
     } else if (process.env.NODE_ENV === 'production') {
-        productionsError(error, res);
+        let errorDes = {...error};
+        if (errorDes.name === 'CastError') errorDes = handleErrorDB(errorDes);
+
+        productionsError(errorDes, res);
     }
 };
 
