f7b615f47f2229ad779452d369eda0db04c7ee5d
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-02-07 20:19:00 +0100

    Authentication with jwt was implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,50 @@
 const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const jwt = require('jsonwebtoken');
 
 const signUp = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
 
-    if (newUser && typeof newUser !== 'undefined') {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                user: newUser
-            }
-        })
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    if (name && typeof name !== 'undefined',
+    email && typeof email !== 'undefined',
+    password && typeof password !== 'undefined',
+    confirmPassword && typeof confirmPassword !== 'undefined'
+    ) {
+        const newUser = await User.create({
+            name: name,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        });
+
+        const token = jwt.sign({
+            id: newUser._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        if (newUser && typeof newUser !== 'undefined' && token && typeof token !== 'undefined') {
+            res.status(201).json({
+                status: 'success',
+                token,
+                data: {
+                    user: newUser
+                }
+            })
+        } else {
+            return next(new AppError('No user could be created', 400));
+        }
     } else {
-        return next(new AppError('No user could be created', 400))
+        return next(new AppError('No user could be created, some fields that you use are wrong', 400));
     }
+
+
 });
-
-
-
-
 
 
 module.exports = {
