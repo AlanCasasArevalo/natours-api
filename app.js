4641234a0c5c04d19ae826cc3675a4d91579ff85
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-01-21 13:52:02 +0100

    [FEATURE][SECURITY] * Data sanitization against XSS and NoSQL Query injection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xssClean = require('xss-clean')
 const helmet = require('helmet')
 const path = require('path');
 const AppError = require('./utils/appError');
@@ -13,6 +15,13 @@ const app = express();
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 app.use(express.json({ limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XXS
+app.use(xssClean())
+
 app.use(express.static(`${__dirname}/public`));
 
 if (process.env.NODE_ENV === 'development') {
