318b98229db11d95bdfa0d16fe3147582d94189d
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-01-21 12:47:58 +0100

    [FEATURE][SECURITY] * Rate limiting was implemented to evicted force brutal attacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
 const path = require('path');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -16,6 +17,15 @@ app.use(express.static(`${__dirname}/public`));
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// Este limitador te obliga a no pasarte de las 100 peticiones en una hora. De hacer mas peticiones de 100 te obliga a esperar 1 hora a volver a realizar peticiones
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour!!!'
+})
+
+app.use('/api', limiter)
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
