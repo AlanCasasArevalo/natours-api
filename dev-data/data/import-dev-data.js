6eff598722715b598a61237b8ae0b9ce80a69dad
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-01-26 15:59:22 +0100

    [FEATURE][IMPORT DEV DATA] * All data will be create or delete if using those commands: node ./dev-data/data/import-dev-data.js --import node ./dev-data/data/import-dev-data.js --delete

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
+
 const dotenv = require('dotenv');
 dotenv.config({
     path: './config.env'
 });
 let DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
+    '<DATABASE_PASSWORD>',
     process.env.DATABASE_PASSWORD
 );
 
@@ -23,11 +26,15 @@ mongoose
 
 //Read Json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // Import all backup data base
 const importData = async () => {
     try {
         await Tour.create(tours)
+        await User.create(users, { validateBeforeSave: false})
+        await Review.create(reviews)
         console.log('Data backup was loaded');
         process.exit();
     } catch (error) {
@@ -39,6 +46,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
       await Tour.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
       console.log('Data was deleted');
       process.exit();
   }  catch (error) {
@@ -47,8 +56,12 @@ const deleteData = async () => {
 };
 if (process.argv[2] === '--import') {
     importData()
+        .then(success => console.log(`Operacion Realizada correctamente`))
+        .catch(error => console.log(`Error al realizar la operacion`))
 } else if (process.argv[2] === '--delete') {
     deleteData()
+        .then(success => console.log(`Operacion Realizada correctamente`))
+        .catch(error => console.log(`Error al realizar la operacion`))
 }
 
 
